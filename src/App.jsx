3d9555c7f27486948b23605b7765d6bde3eb6c7d
alokvns48 <alokvns48.com>
2024-10-08 05:24:35 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import TextAnalysisApp from "./components/TextAnalysisApp";
-import Alok from "./Alok";
+
 function App() {
   return (
     <div>
       <TextAnalysisApp />
-      {/* <Alok/> */}
     </div>
   );
 }
