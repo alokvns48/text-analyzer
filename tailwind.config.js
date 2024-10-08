7601382f41478b139067d6ec0f4fc39422056229
alokvns48 <alokvns48.com>
2024-10-08 05:49:41 +0530

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,36 @@ export default {
     extend: {},
   },
   plugins: [],
+  safelist: [
+
+    // Background color classes
+    "bg-blue-500",
+    "bg-yellow-500",
+    "bg-green-500",
+    'bg-blue-100',
+    'bg-green-100',
+    'bg-yellow-100',
+
+
+    // Focus color classes
+    "focus:ring-blue-500",
+    "focus:ring-yellow-500",
+    "focus:ring-green-500",
+
+    // Hover color classes
+    "hover:bg-yellow-600",
+    "hover:bg-green-600",
+    "hover:bg-blue-600",
+
+     // Text color classes
+     'text-blue-800',
+     'text-green-800',
+     'text-yellow-800',
+
+     // Value color classes
+     'text-blue-600',
+     'text-green-600',
+     'text-yellow-600',
+
+  ],
 };
